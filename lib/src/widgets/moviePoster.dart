619dcbd30bdccb218a735e28ee26e8b553d91817
Daniel Beltran <danielesbe9526@hotmail.comm>
2020-07-05 15:03:30 -0500

    added recomendatiosn by moview and button to add wach list

--- lib/src/widgets/moviePoster.dart
+++ lib/src/widgets/moviePoster.dart
@@ -3,29 +3,32 @@ import 'package:WFHchallenge/src/models/page_model.dart';
 import 'package:flutter/material.dart';
 
 class MoviePoster extends StatelessWidget {
-  
   final double heigthMovie = 145;
   final double widthMovie = 99;
   final double _widthRating = 48;
   final Color _darkBlue = Color.fromRGBO(22, 25, 29, 1);
   final Color _blue = Color.fromRGBO(28, 31, 44, 1);
   final Color _orange = Color.fromRGBO(235, 89, 25, 1);
-  final BoxShadow boxShadow = BoxShadow( color: Colors.black26, blurRadius: 10.0, spreadRadius: 2.0, offset: Offset(2.0,10.0));
+  final BoxShadow boxShadow = BoxShadow(
+      color: Colors.black26,
+      blurRadius: 10.0,
+      spreadRadius: 2.0,
+      offset: Offset(2.0, 10.0));
   final BorderRadius borderRadius = BorderRadius.circular(6.0);
 
   final MovieModel movie;
-  MoviePoster({ @required  this.movie});
+  MoviePoster({@required this.movie});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:  Column(
-        children: <Widget>[   
+      child: Column(
+        children: <Widget>[
           Stack(
             children: <Widget>[
               ClipRRect(
                 child: FadeInImage(
-                  placeholder: AssetImage('assets/defaultcover.png'), 
+                  placeholder: AssetImage('assets/defaultcover.png'),
                   image: NetworkImage(movie.posterPath),
                   height: heigthMovie,
                   width: widthMovie,
@@ -36,25 +39,54 @@ class MoviePoster extends StatelessWidget {
               Container(
                 width: widthMovie,
                 height: 145,
-                decoration:  BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.transparent, _blue],
-                    stops: [0.0,1],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  )
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [Colors.transparent, _blue],
+                  stops: [0.0, 1],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                )),
+              ),
+              Positioned(
+                child: Container(
+                  child: GestureDetector(
+                    child: Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 10.5,
+                    ),
+                    onTap: () {
+                      print('Added to watch list');
+                    },
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(0, 0, 0, 0.9),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(14),
+                        topLeft: Radius.circular(14)),
+                  ),
+                  width: 23,
+                  height: 24,
                 ),
+                top: 14,
+                right: 0,
               ),
               Positioned(
                 bottom: 11,
-                left: (widthMovie/2) - (_widthRating/2),
+                left: (widthMovie / 2) - (_widthRating / 2),
                 child: Container(
                   child: Row(
                     children: <Widget>[
                       Spacer(),
-                      Text(movie.rating.toStringAsFixed(1),style: TextStyle(color: Colors.white, fontSize: 11),),
+                      Text(
+                        movie.rating.toStringAsFixed(1),
+                        style: TextStyle(color: Colors.white, fontSize: 11),
+                      ),
                       Container(
-                        child: Image.asset('assets/Star.png',color: Colors.white,),
+                        child: Image.asset(
+                          'assets/Star.png',
+                          color: Colors.white,
+                        ),
                         width: 7.2,
                         height: 7.2,
                         padding: EdgeInsets.only(left: 1),
@@ -65,9 +97,7 @@ class MoviePoster extends StatelessWidget {
                   width: 48,
                   height: 17,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    color: _orange
-                  ),
+                      borderRadius: BorderRadius.circular(14), color: _orange),
                 ),
               ),
             ],
@@ -89,7 +119,7 @@ class MoviePoster extends StatelessWidget {
           ),
         ],
       ),
-      margin: EdgeInsets.only(left: 10,right: 10),
+      margin: EdgeInsets.only(left: 10, right: 10),
     );
   }
 }
